ebbd3fe2a9d6b223fd5e4829de745aae95f87aaf
kmelct <kmelct@gmail.com>
2019-12-21 21:50:19 +0200

    develop: impliment main blockchain funcs

--- src/modules/blockchain/services/blockchain.service.ts
+++ src/modules/blockchain/services/blockchain.service.ts
@@ -1,5 +1,51 @@
 // Core
 import { Injectable } from '@nestjs/common';
 
+interface ITransaction {
+  sender: string;
+  recipient: string;
+  amount: number;
+}
+interface IBlock {
+  index: number;
+  timestamp: number;
+  transactions: ITransaction[];
+  proof: number;
+  previousHash: string;
+}
+
 @Injectable()
-export class BlockchainService {}
+export class BlockchainService {
+  private readonly chain: IBlock[] = [];
+  private currentTransactions: ITransaction[] = [];
+
+  public newBlock(proof: number, previousHash: string): IBlock {
+    const block = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.currentTransactions,
+      proof,
+      previousHash,
+    };
+
+    this.currentTransactions = [];
+    this.chain.push(block);
+
+    return block;
+  }
+
+  public newTransaction(transaction: ITransaction) {
+    const { sender, recipient, amount } = transaction;
+
+    this.currentTransactions.push({ sender, recipient, amount });
+
+    return this.lastBlock().index;
+  }
+
+  public hash(block: IBlock) {}
+
+  public lastBlock() {
+    const length = this.chain.length;
+    return this.chain[length - 1];
+  }
+}
